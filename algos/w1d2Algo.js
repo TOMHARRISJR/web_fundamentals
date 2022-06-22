c35be33420fb5df286188abebb3569b68d051448
TOMHARRISJR <harristome@gmail.com>
2022-06-22 11:36:34 -0700

    added

---
+++ algos/w1d2Algo.js
@@ -0,0 +1,45 @@
+var name ="Tom Harris";
+var age = 33;
+var chipotlePrice = 10.55;
+var isLearningToday = true;
+
+
+// console.log("burrito " +name);
+
+chipotlePrice += 1;
+
+// console.log(chipotlePrice);
+
+var halfMyAge = age/2;
+var twiceMyAge = age *2;
+
+
+// console.log(halfMyAge);
+
+
+
+var artists = ["drake", "jay z", "ice cube"]; // 3 elements in this array
+// indexes->      0        1         2
+
+
+console.log(artists[0,1,2])
+
+
+for(var i = 0; i<=20; i++){
+    console.log(i)
+}
+
+
+
+
+
+
+
+
+
+
+// camelCase
+// snake_Case
+// PascalCase
+
+
