065d6ed47257e7f6bce63507410a1fa647783147
TOMHARRISJR <harristome@gmail.com>
2022-06-24 12:51:54 -0700

    jane doe

---
+++ algos/w1d5Algo.js
@@ -0,0 +1,22 @@
+function greetUser(firstName,lastName){
+    console.log("Hello " + firstName + " " + lastName);
+}
+
+// greetUser("Tom","Harris")
+
+// Parameter->
+
+// THE VALUE OF A FUNCTION CALL IS WHATEVER FUNCTION CALL RETURNS!
+
+
+let x + 5;
+
+function sum(num1,num2){
+    return num1 + num2
+}
+
+let someNum = sum(3,9) + sum(1,2);
+
+console.log(sumNum)
+
+console.log(someNum)
